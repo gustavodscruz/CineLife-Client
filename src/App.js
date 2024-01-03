047b617e16ed140f3874179c44dfae8041b34c73
gustavodscruz <gustavodiasdsc@gmail.com>
2024-01-02 23:19:49 -0300

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   const [movies, setMovies] = useState();
@@ -8,6 +11,7 @@ function App() {
   const getMovies = async () => {
     try{
       const response = await api.get("/api/v1/movies");
+      console.log(response.data);
 
     setMovies(response.data);
     }catch(error){
@@ -16,11 +20,19 @@ function App() {
 
 
     
-  };
+  };  
+
+  useEffect(() => {
+    getMovies();
+  })
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path='/' element={<Home/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
